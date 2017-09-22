95e53c384395dd44c0a59ae3d2542f86a79a562e
Stephen <kmng@ucla.edu>
2017-09-22 11:35:24 -0700

    sent security token correctly

--- src/app/components/todo/todo.directive.js
+++ src/app/components/todo/todo.directive.js
@@ -3,13 +3,28 @@
 
     angular
         .module('angularSeedApp')
+        .factory('authInterceptor', authInterceptor)
+        .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptor');
+        })
         .directive('todo', todo);
 
+    function authInterceptor  ($rootScope, $window) {
+        return {
+            request: function (config) {
+                config.headers = config.headers || {};
+                if ($window.sessionStorage.token) {
+                    config.headers['X-Auth-Token'] =  $window.sessionStorage.token;
+                }
+                return config;
+            }
+        };
+    }
     /** @ngInject */
     function todo() {
         var directive = {
             restrict: 'E',
-            templateUrl: 'app/components/todo/todo.html',            
+            templateUrl: 'app/components/todo/todo.html',
             controller: TodoController,
             controllerAs: 'vm',
             bindToController: true
@@ -18,11 +33,13 @@
         return directive;
 
         /** @ngInject */
-        function TodoController(Todo) {
+        function TodoController(Todo, $window) {
+
+
 	   	var vm = this;
 
-    		vm.todos = Todo.list();	
-		
+    		vm.todos = Todo.list();
+
 		vm.newTodo = new Todo();
 
     vm.save = function() {
