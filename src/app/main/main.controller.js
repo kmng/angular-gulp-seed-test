0ea1db557c2236b13859fb33347812256401aa4c
Stephen <kmng@ucla.edu>
2017-09-22 11:21:17 -0700

    add login form and login process

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,5 +6,19 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController() {}
+    function MainController($window,$http) {
+       var main = this;
+       main.authenticated = false;
+       main.user = {};
+
+       main.login = function () {
+         $http.post('http://192.168.86.137:8080/api/login', {
+             username: main.user.username,
+             password: main.user.password
+         }).then(function (response) {
+             main.authenticated = true;
+             $window.sessionStorage.token = response.data.access_token;
+         });
+     };
+    }
 })();
